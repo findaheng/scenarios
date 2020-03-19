b7b8b9b5ff66184734267d07b874e7179577374a
Francis Indaheng <findaheng@berkeley.edu>
2020-03-19 13:30:37 -0700

    ground_truths done

--- ground_truths.py
+++ ground_truths.py
@@ -1,11 +1,38 @@
 import carla
 
-def prologue():
+HOST = 'localhost'
+PORT = 2000
+
+def main():
+
+	# Initialize CARLA client
+	client = carla.Client(HOST, PORT)
+	client.set_timeout(2.0)
+
+	# Retrieve world, map, and blueprint library
+	world = client.get_world()
+	map_ = world.get_map()
+	blueprint_library = world.get_blueprint_library()
+
+	# Set to asynchronous with a fixed timestep
 	settings = world.get_settings()
-	settings.synchronous_mode = True
+	settings.fixed_delta_seconds = 0.05
+	settings.synchronous_mode = False
 	world.apply_settings(settings)
 
-def extract_ground_truths(objects=[], numTimesteps=100):
-	''' Extracts ground truths at each timestep of Carla'''
+	# Randomly spawn a vehicle
+	bp = random.choice(blueprint_library.filter('vehicle'))
+	if bp.has_attribute('color'):
+		color = random.choice(bp.get_attribute('color').recommended_values)
+		bp.set_attribute('color', color)
+	transform = random.choice(map_.get_spawn_points())
+	vehicle = world.spawn_actor(bp, transform)
 
-	f
+	for step in range(10):
+		print(f'\nTimestep: {step}\n')
+		snapshot = world.wait_for_tick()
+		print(snapshot)
+
+if __name__ == '__main__':
+	main()
+	
